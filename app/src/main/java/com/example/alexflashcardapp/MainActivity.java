fac0ba57cd48ed8922fa21ed90f163b755f49189
Alex Cheung <alexcheung880@gmail.com>
2022-02-26 11:50:26 -0800

    initial commit

---
+++ app/src/main/java/com/example/alexflashcardapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.alexflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+
+
+}
