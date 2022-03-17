07bf3a1650e5555677425b412eb81cad63c1d814
Alex Cheung <alexcheung880@gmail.com>
2022-03-16 22:18:30 -0700

    week 4 lab 2 flashcard app updated the save buttons can go back and forth between question and answer

---
+++ app/src/main/java/com/example/alexflashcardapp/AddCardActivity.java
@@ -0,0 +1,43 @@
+package com.example.alexflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.flashcard_cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.flashcard_save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data = new Intent();
+                String inputQuestion = ((EditText) findViewById(R.id.input_question)).getText().toString();
+                String inputAnswer = ((EditText) findViewById(R.id.input_answer)).getText().toString();
+                data.putExtra("question_key", inputQuestion);
+                data.putExtra("answer_key", inputAnswer);
+                setResult(RESULT_OK, data);
+                finish();
+
+            }
+        });
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/alexflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/alexflashcardapp/MainActivity.java
@@ -1,26 +1,82 @@
 package com.example.alexflashcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+// my added
+import android.content.Intent;
+import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView questionTextView;
+    TextView answerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener(){
+
+        findViewById(R.id.flashcard_add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
 
+
+        questionTextView = findViewById(R.id.input_question);
+        answerTextView = findViewById(R.id.input_answer);
+
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+
+                //Toast.makeText(MainActivity.this, "I clicked the question!", Toast.LENGTH_SHORT).show();
+                //Log.i("Alex", "entered question onclick method");
+            }
+        });
+
+        answerTextView.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setVisibility(View.INVISIBLE);
+                questionTextView.setVisibility(View.VISIBLE);
+            }
+        }));
+
+
+
+        ImageView addQuestionImageView = findViewById(R.id.flashcard_add_button);
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivityForResult(intent, 100);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            // get data
+            if (data != null) {
+                String questionString = data.getExtras().getString("question_key");
+                String answerString = data.getExtras().getString("answer_key");
+                questionTextView.setText(questionString);
+                answerTextView.setText(answerString);
+            }
+        }
+    }
 
 }
