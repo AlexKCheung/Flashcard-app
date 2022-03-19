1e8a07a06877944250aa72178652731159115f51
Alex Cheung <alexcheung880@gmail.com>
2022-03-19 11:40:11 -0700

    done with required features for app, added my own previous back button for naviagting flashcards

--- app/src/main/java/com/example/alexflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/alexflashcardapp/MainActivity.java
@@ -11,15 +11,96 @@ import android.content.Intent;
 import android.widget.Toast;
 import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import org.w3c.dom.Text;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView questionTextView;
     TextView answerTextView;
 
+    // outside of oncreate, create flashcarddatabase
+    FlashcardDatabase flashcardDatabase;
+
+    // in mainactivity: hold our flashcard objects, outside of oncreate hold list of flashcards
+    List<Flashcard> allFlashcards;
+
+    // init variable for card display
+    int currentCardDisplayIndex = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // inside ooncreate initalize flashcarddatabase variable
+        // inside oncreate because getapplicationcontext will return null if app not init yet
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext()); // this instead of getappcontext?
+
+        // check if any data is in our database to be displayed / read database when app launched
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        // if statement, check if list empty or not, display saved flashcard or default from xml
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.input_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.input_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
+
+        // next button to view saved flashcards
+        findViewById(R.id.flashcard_next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // base case
+                if (allFlashcards.size() == 0) {
+                    return;
+                }
+                // advance pointer to next index to show next card
+                currentCardDisplayIndex += 1;
+
+                // make sure not index out of bounds if viewing last card in list
+                if (currentCardDisplayIndex >= allFlashcards.size()) {
+                    // reset
+                    currentCardDisplayIndex = 0;
+                }
+
+                // set question and answer textviews with data form database
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayIndex);
+
+                ((TextView) findViewById(R.id.input_question)).setText(flashcard.getQuestion());
+                ((TextView) findViewById(R.id.input_answer)).setText(flashcard.getAnswer());
+            }
+        });
+        // PREVIOUS button to view saved flashcards
+        findViewById(R.id.flashcard_prev_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // base case
+                if (allFlashcards.size() == 0) {
+                    return;
+                }
+                // advance pointer to next index to show next card
+                currentCardDisplayIndex -= 1;
+
+                // make sure not index out of bounds if viewing last card in list
+                if (currentCardDisplayIndex < 0) {
+                    // reset
+                    currentCardDisplayIndex = allFlashcards.size() - 1;
+                }
+
+                // set question and answer textviews with data form database
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayIndex);
+
+                ((TextView) findViewById(R.id.input_question)).setText(flashcard.getQuestion());
+                ((TextView) findViewById(R.id.input_answer)).setText(flashcard.getAnswer());
+            }
+        });
+
+
 
 
         findViewById(R.id.flashcard_add_button).setOnClickListener(new View.OnClickListener() {
@@ -75,6 +156,13 @@ public class MainActivity extends AppCompatActivity {
                 String answerString = data.getExtras().getString("answer_key");
                 questionTextView.setText(questionString);
                 answerTextView.setText(answerString);
+
+                // grab data to save into database
+                flashcardDatabase.insertCard(new Flashcard(questionString, answerString));
+
+                // local variable holding list of flashcards update
+                allFlashcards = flashcardDatabase.getAllCards();
+
             }
         }
     }
